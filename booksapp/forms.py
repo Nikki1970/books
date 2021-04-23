75280977b4301aafc7965b13490446b3b1ca3de1
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-23 18:41:21 +0530

    Use forms

--- booksapp/forms.py
+++ booksapp/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Book
+from .models import Book, Genre
 
 class BookForm(forms.ModelForm):
 
     class Meta:
         model = Book
         fields = ('title','no_of_pages','isbn','image','description','genre','publishers','published_date')
+
+class GenreForm(forms.ModelForm):
+
+    class Meta:
+        model = Genre
+        fields = ('name',)
+        
