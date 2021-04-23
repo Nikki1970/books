1813c0aea8736a5d6873e345608e306ce7bb02f9
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-23 18:40:58 +0530

    add,delete,update publisher field

--- booksapp/urls.py
+++ booksapp/urls.py
@@ -6,4 +6,12 @@ urlpatterns=[
     path('new/', views.book_new, name='new_book'),
     path('delete/<int:pk>/',views.deleteBook, name="delete_book"),
     path('update/<int:pk>/',views.updateBook, name="update_book"),
+    path('genre/',views.GenreListView.as_view(),name="list-of-genres"),
+    path('genrenew/',views.create_genre, name = "new_genre"),
+    path('updategenre/<int:pk>/',views.update_genre, name="update_genre"),
+    path('delete/genre/<int:pk>/', views.Genredeleteview.as_view(), name = 'delete_genre'),
+    path('publishers/',views.PublisherListView.as_view(), name='list-of-publishers'),
+    path('newpublisher/',views.PublisherCreateView.as_view(), name='new_publisher'),
+    path('updatepublisher/<int:pk>/', views.Publisherupdateview.as_view(),name='update_publisher'),
+    path('delete/publisher/<int:pk>/', views.Publisherdeleteview.as_view(),name='delete_publisher'),
 ]
--- booksapp/views.py
+++ booksapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
-from booksapp.models import Book
+from booksapp.models import Book, Genre, Publisher
 from django.views import generic
-from .forms import BookForm
+from .forms import BookForm, GenreForm
 from .filters import BookFilter
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 # Create your views here.
+
+
 class BookListView(generic.ListView):
     model = Book
 
@@ -14,13 +16,16 @@ class BookListView(generic.ListView):
         context['filter'] = BookFilter(self.request.GET,queryset=self.get_queryset())
         return context
 
+
 class BookDetailView(generic.DetailView):
     model = Book
+
 
 def book_new(request):
     form = BookForm(request.POST or None)
     if form.is_valid():
-        form = form.save(commit=False)
+        form = form.save()
+        form.author = request.user
         form.save()
         return redirect('list-of-books')
     context = {
@@ -28,13 +33,15 @@ def book_new(request):
     }
     return render(request, 'booksapp/book_edit.html',context)
 
+
 def deleteBook(request, pk):
-    instance = Book.objects.get(id=pk)
+    book = Book.objects.get(id=pk)
     if request.method =="POST":
-        instance.delete()
+        book.delete()
         return redirect('list-of-books')
-    context = {'form': instance,'book':instance}
+    context = {'book':book}
     return render(request, "booksapp/book_delete.html",context)
+
 
 def updateBook(request,pk):
     book = Book.objects.get(id=pk)
@@ -46,3 +53,55 @@ def updateBook(request,pk):
         'form': form,
     }    
     return render(request, "booksapp/book_edit.html", context)
+
+
+class GenreListView(generic.ListView):
+    model = Genre
+
+def create_genre(request):
+    form = GenreForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list-of-genres')
+    context = {
+        'form': form
+    }
+    return render(request, 'booksapp/genre_edit.html',context)
+
+
+def update_genre(request,pk):
+    genre = Genre.objects.get(id = pk)
+    form = GenreForm(request.POST or None, instance = genre)
+    if form.is_valid():
+        form.save()
+        return redirect('list-of-genres')
+    context = {
+        'form': form,
+    }    
+    return render(request, "booksapp/genre_edit.html", context)
+
+
+class Genredeleteview(generic.DeleteView):
+    model = Genre
+    success_url = reverse_lazy('list-of-genres')
+
+
+class PublisherListView(generic.ListView):
+    model = Publisher
+
+
+class PublisherCreateView(generic.CreateView):
+    model = Publisher
+    fields = ['name']
+    success_url = reverse_lazy('list-of-publishers')
+
+
+class Publisherupdateview(generic.UpdateView):
+    model = Publisher
+    fields = ['name']
+    success_url = reverse_lazy('list-of-publishers')
+
+
+class Publisherdeleteview(generic.DeleteView):
+    model = Publisher
+    success_url = reverse_lazy('list-of-publishers')
