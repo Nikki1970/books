0c5929ade7d6d96812e2d9ec889bb9ef5f28f6cb
Nikki1970 <sagalanikhilesh99@gmail.com>
2021-04-23 18:41:51 +0530

    Filter Books using Publication Date,genre,author and publisher

--- booksapp/filters.py
+++ booksapp/filters.py
@@ -1,15 +1,13 @@
 import django_filters
 from django_filters import DateFilter
-from .models import Book
+from .models import Book, Genre
 
 class BookFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='icontains')
     choice_day = tuple((x, x) for x in range(1, 32))
     choice_month = tuple((x, x) for x in range(1, 13))
     day = django_filters.ChoiceFilter(field_name="published_date",lookup_expr='day',label="Day",choices=choice_day)
     month = django_filters.ChoiceFilter(field_name='published_date', lookup_expr='month', label="Month",choices=choice_month)
     year = django_filters.NumberFilter(field_name='published_date', lookup_expr='year', label="Year")
-
     class Meta:
         model = Book
-        fields = ['title','author']    
+        fields = ['author','publishers','genre']
